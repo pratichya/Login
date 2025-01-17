7f2bdcf4c80821f34d49a4662ec79c73e7e24fd1
Pratichya Giri <134180783+pratichya@users.noreply.github.com>
2025-01-17 19:42:35 +0545

    Update save_password.php

--- save_password.php
+++ save_password.php
@@ -5,11 +5,10 @@ if (!empty($username) && !empty($password)) {
     header("Location:login.php");
 }
     if (isset($_POST['save_password']) && $_POST['save_password'] == 'on') {
-        // Retrieve username and password from the form
+        // Retrieving username and password from the form
         $username = $_POST['username'];
         $password = $_POST['password'];
-
-        // Prepare and execute SQL statement to insert data into the database
+        
         $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
         if ($conn->query($sql) === TRUE) {
             echo "New record created successfully";
